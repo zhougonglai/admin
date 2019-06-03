ff44e954c8a2162c5514a428accc4a4547693825
周公来 <zhougonglai@dingtalk.com>
2019-06-03 21:59:00 +0800

    type: fix

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
     canActivate: [SimpleGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', redirectTo: 'dashboard' },
       {
         path: 'dashboard',
         loadChildren: './dashboard/dashboard.module#DashboardModule',
